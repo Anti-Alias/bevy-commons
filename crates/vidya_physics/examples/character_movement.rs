8e8745f0d1d299c003d6832eb365ed81943379bf
William Cahill <drewlikesramen@gmail.com>
2022-09-02 22:36:08 -0400

    Built playground

--- crates/vidya_physics/examples/character_movement.rs
+++ crates/vidya_physics/examples/character_movement.rs
@@ -1,134 +1,106 @@
-use vidya_camera_target::prelude::*;
-use vidya_fixed_timestep::{CurrentTransform, PreviousTransform, FixedTimestepPlugin};
-use vidya_physics::*;
-use bevy::prelude::*;
+//#[cfg(feature = "debug")]
+mod example {
+    use vidya_fixed_timestep::FixedTimestepPlugin;
+    use vidya_physics::*;
+    use vidya_physics::debug::*;
+    use bevy::prelude::*;
 
-// Marks ball entity
-#[derive(Component, Debug)]
-struct Ball;
 
-// Wall constants
-const FLOOR: f32 = 0.0;
-const LEFT_WALL: f32 = -5.0;
-const RIGHT_WALL: f32 = 5.0;
-const NEAR_WALL: f32 = 5.0;
-const FAR_WALL: f32 = -5.0;
-const JUMP_SPEED: f32 = 0.2;
+    /// Example where three chunks are spawned side by side.
+    /// Chunks are not rotated via a [`Transform`], but rather are constructed differently.
+    pub fn start() {
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .add_plugin(FixedTimestepPlugin::default())
+            .add_plugin(PhysicsPlugin)
+            .add_plugin(PhysicsDebugPlugin)         // Added to enable debug rendering, in this case, for the chunks
+            .add_startup_system(startup)
+            .run();
+    }
 
-/// Example where only a single falling entity is spawned.
-pub fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(FixedTimestepPlugin::default())
-        .add_plugin(PhysicsPlugin)
-        .add_plugin(CameraTargetPlugin)
-        .add_startup_system(startup)
-        .add_system(bounce_ball)
-        .run();
-}
+    /// Spawns light, chunks and camera
+    fn startup(mut commands: Commands) {
 
-fn startup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
-) {
-
-    // Adds gravity
-    commands.insert_resource(Gravity(Vec3::new(0.0, -0.01, 0.0)));
-
-    // Spawns light above scene
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 10.0, 0.0),
-        ..default()
-    });
-
-    // Spawns plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(shape::Plane { size: 10.0 }.into()),
-        material: materials.add(Color::GREEN.into()),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    });
-
-    // Spawns player
-    let start_transform = Transform::from_xyz(0.0, 0.5, 0.0);
-    let player = commands
-        .spawn()
-        .insert_bundle(PbrBundle {
-            mesh: meshes.add(shape::Box::new(1.0, 1.0, 1.0).into()),
-            material: materials.add(Color::RED.into()),
-            ..default()
-        })
-        .insert_bundle(
-            PhysicsBundle {
-                current_transform: CurrentTransform(start_transform),
-                previous_transform: PreviousTransform(start_transform),
-                bounds: Bounds::new(Vec3::new(1.0, 1.0, 1.0)),
-                velocity: Velocity(Vec3::new(0.05, JUMP_SPEED, 0.025)),
+        // Spawns light above scene
+        commands.spawn_bundle(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
                 ..default()
-            }
-        )
-        .insert(Ball)
-        .id();
-
-    // Spawns camera
-    commands
-        .spawn()
-        .insert_bundle(Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 2.0, 10.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
-            ..default()
-        })
-        .insert_bundle(CameraTargetBundle {
-            target: Target::Entity(player),
-            target_style: TargetStyle::Offset(Vec3::new(0.0, 7.0, 7.0)),
+            },
+            transform: Transform::from_xyz(7.0, 7.0, 7.0),
             ..default()
         });
+
+        // Spawns 3 voxel chunks, all double pyramids of a different orientation
+        let chunk = generate_chunk();
+        commands.spawn_bundle(VoxelChunkBundle::new(
+            chunk,                                      // Raw chunk data
+            Transform::from_xyz(0.0, 0.0, 0.0),         // Center of the chunk in units
+            Bounds::new(Vec3::new(8.0, 2.0, 8.0))       // Size of the chunk in units
+        )).insert(DebugRender);                         // Allows debug info of chunk to be rendered
+
+        // Spawns camera
+        commands.spawn()
+            .insert_bundle(Camera3dBundle {
+                transform: Transform::from_xyz(0.0, 2.0, 7.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+                ..default()
+            });
+    }
+
+    fn generate_chunk() -> VoxelChunk {
+        let mut chunk = VoxelChunk::new(UVec3::new(16, 4, 16));
+        chunk
+            // Bottom plane
+            .set_voxel_plane(
+                0,
+                UVec2::new(0, 0),
+                UVec2::new(16, 16),
+                PlaneAxis::XZ,
+                VoxelData::new(Voxel::Cuboid)
+            )
+            // Left rim
+            .set_voxel_plane(
+                1,
+                UVec2::new(0, 0),
+                UVec2::new(1, 16),
+                PlaneAxis::XZ,
+                VoxelData::new(Voxel::Cuboid)
+            )
+            // Right rim
+            .set_voxel_plane(
+                1,
+                UVec2::new(15, 0),
+                UVec2::new(16, 16),
+                PlaneAxis::XZ,
+                VoxelData::new(Voxel::Cuboid)
+            )
+            // Near rim
+            .set_voxel_plane(
+                1,
+                UVec2::new(1, 15),
+                UVec2::new(15, 16),
+                PlaneAxis::XZ,
+                VoxelData::new(Voxel::Cuboid)
+            )
+            // Far rim
+            .set_voxel_plane(
+                1,
+                UVec2::new(1, 0),
+                UVec2::new(15, 1),
+                PlaneAxis::XZ,
+                VoxelData::new(Voxel::Cuboid)
+            );
+        chunk
+    }
 }
 
-fn bounce_ball(mut entities: Query<
-    (
-        &mut CurrentTransform,
-        &Bounds,
-        &mut Velocity
-    ),
-    With<Ball>>
-) {
-    for (mut trans, bounds, mut vel) in &mut entities {
+#[cfg(feature = "debug")]
+fn main() {
+    example::start();
+}
 
-        // Bounces off floor
-        let trans = &mut trans.0.translation;
-        if trans.y - bounds.half_extents.y <= FLOOR {
-            trans.y = FLOOR + bounds.half_extents.y;
-            vel.0.y = JUMP_SPEED;
-        }
-
-        // Bounces off left wall
-        if trans.x - bounds.half_extents.x <= LEFT_WALL {
-            trans.x = LEFT_WALL + bounds.half_extents.x;
-            vel.0.x *= -1.0;
-        }
-
-        // Bounces off right wall
-        if trans.x + bounds.half_extents.x >= RIGHT_WALL {
-            trans.x = RIGHT_WALL - bounds.half_extents.x;
-            vel.0.x *= -1.0;
-        }
-
-        // Bounces off near wall
-        if trans.z + bounds.half_extents.z >= NEAR_WALL {
-            trans.z = NEAR_WALL - bounds.half_extents.z;
-            vel.0.z *= -1.0;
-        }
-
-        // Bounces off far wall
-        if trans.z - bounds.half_extents.z <= FAR_WALL {
-            trans.z = FAR_WALL + bounds.half_extents.z;
-            vel.0.z *= -1.0;
-        }
-    }
+#[cfg(not(feature = "debug"))]
+fn main() {
+    panic!("Feature 'debug' not set");
 }
